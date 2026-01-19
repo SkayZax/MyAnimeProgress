310fa4a5885de6ab2c3921b16d8b8f6dad78867d
mathis_poirier <mathis_poirier@ens.univ-artois.fr>
2026-01-19 09:23:44 +0100

    bouton page suivante et precedente faite style fait avec ia car bug

--- app.py
+++ app.py
@@ -1,13 +1,14 @@
-from flask import Flask, render_template
-
+from flask import Flask, render_template, request
 from api.API import API
 app=Flask(__name__)
 api=API()
 @app.route("/", methods=["GET"])
 def index():
-    animes = api.get_animepage(1)
-    images = api.images(animes[0])
-    return render_template("index.html", animes=animes, images=images)
+    page = request.args.get('page', 1, type=int)
+
+    animes = api.get_animepage(page)
+
+    return render_template("index.html", animes=animes, current_page=page)
 
 
 
